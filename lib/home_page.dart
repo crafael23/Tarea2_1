64256110c4cee696b93bea95e3e270c9a7cb10cf
omarpadilla1472 <omarpadilla1472@gmail.com>
2024-07-14 04:36:55 +0000

    re doing some stuff

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,47 +1,30 @@
-import 'package:flutter/material.dart';
-
-class HomePage extends StatefulWidget {
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2314077104.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2747082835.
+class HomePage extends StatelessWidget {
   const HomePage({super.key, this.name = '', this.email = ''});
 
   final String name;
   final String email;
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String username = '';
-  String useremail = '';
-
-  @override
-  void initState() {
-    super.initState();
-
-    username = widget.name;
-    useremail = widget.email;
-  }
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              username,
-              style: const TextStyle(fontSize: 24),
+      body: Row(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Username: $name'),
+                Text('Email: $email'),
+              ],
             ),
-            const SizedBox(height: 20),
-            Text(
-              useremail,
-              style: const TextStyle(fontSize: 24),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,34 @@
 import 'package:flutter/material.dart';
-//yo hice esta pagina, pero en el mismo workspace por lo cual aparece que claudio como autor :/
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, this.name = '', this.email = ''});
+import 'package:myapp/widgets/login_page.dart';
+import 'home_page.dart';
+import 'widgets/registro_page.dart';
 
-  final String name;
-  final String email;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
+void main() {
+  runApp(MyApp());
 }
 
-class _HomePageState extends State<HomePage> {
-  String username = '';
-  String useremail = '';
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final users = {
+    'claudio.vasquez@unah.hn ': '20192002377',
+    'omar.diaz@unah.hn': '20192001399',
+  };
 
   @override
-  void initState() {
-    super.initState();
-
-    username = widget.name;
-    useremail = widget.email;
-  }
-
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-        ),
-        body: Row(
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Username: $username'),
-                  Text('Email: $useremail'),
-                ],
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, 'registro');
-          },
-          child: const Icon(Icons.add),
-        ));
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: 'login',
+      routes: {
+        "login": (context) => const LoginPage(),
+        'home': (context) => const HomePage(),
+        'registro': (context) => RegistroPage(),
+      },
+    );
   }
 }
